4daf9f9cc5fa60afb905f2a0971a8f77493abe40
Alex Messina <me@alexmessina.dev>
2025-06-16 10:38:33 -0500

    Connected app to Gemini and found it functional. -Functions added for ease of starting up the server and frontend

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,26 +1,24 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-import openai
+from google import genai
 import os
 
 @api_view(["POST"])
 def ask_openai(request):
     prompt = request.data.get("prompt", "")
-    api_key = os.getenv("OPENAI_KEY")
+    api_key = os.getenv("GEMINI_KEY")
 
     if not api_key:
         return Response({"error": "OpenAI API key is not set."}, status=500)
 
-    client = openai.OpenAI(api_key=api_key)
+    client = genai.Client(api_key=api_key)
 
     try:
-        response = client.chat.completions.create(
-            model="o3",
-            messages=[
-                {"role": "user", "content": prompt}
-            ]
+        response = client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents= prompt
         )
-        message = response.choices[0].message.content
+        message = response.text
         return Response({"response": message})
 
     except Exception as e:
--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -16,7 +16,7 @@ import os
 
 load_dotenv()
 
-OPENAI_KEY = os.getenv("OPENAI_API_KEY")
+GEMINI_KEY = os.getenv("GEMINI_KEY")
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 DEBUG = os.getenv("DEBUG") == "True"
 
