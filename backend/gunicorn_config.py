6148ae374f7df4c22267592ee20ec4a349e26f5b
Alex Messina <me@alexmessina.dev>
2025-07-16 13:18:50 -0500

    More edits to get production working - issue is slow Gemini response => alter gunicorn

--- backend/gunicorn_config.py
+++
@@ -1,5 +0,0 @@
-# Create settings for Gunicorns to operate
-bind = "0.0.0.0:8000"
-workers = 2
-worker_class = "uvicorn.workers.UvicornWorker"
-timeout = 60
--- backend/render_start.sh
+++ backend/render_start.sh
@@ -1,5 +1,5 @@
 # Define execution order for render to start
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn backend_project.wsgi:application --bind 0.0.0.0:$PORT
-gunicorn backend_project.asgi:application -c gunicorn_config.py
+gunicorn backend_project.asgi:application --worker-class uvicorn.workers.UvicornWorker --timeout 600 --workers 1 --bind 0.0.0.0:$PORT
+
