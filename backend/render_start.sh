b4dafa1c47a37a04f8373610da788d99ae112765
Alex Messina <me@alexmessina.dev>
2025-07-16 11:30:07 -0500

    Continued to add time (5min) to prevent timeout issue

--- backend/render_start.sh
+++ backend/render_start.sh
@@ -2,4 +2,4 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
 gunicorn backend_project.wsgi:application --bind 0.0.0.0:$PORT
-gunicorn backend_project.wsgi:application --timeout 120
+gunicorn backend_project.wsgi:application --timeout 600
